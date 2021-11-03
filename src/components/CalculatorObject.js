5d114da20027ba6a75e339b8f43e7d1f12adbff5
dialectrical <michael.camele@gmail.com>
2021-11-03 13:50:09 -0700

    feat: negative() switches number between negative and not

--- src/components/CalculatorObject.js
+++ src/components/CalculatorObject.js
@@ -15,6 +15,25 @@ function CalculatorObject() {
     }
   };
 
+  this.decimal = () => {
+    //add a decimal point to current input. If no input yet, set input to "0.". Multiple decimal points in same number should not be allowed.
+  };
+
+  this.negative = () => {
+    if (this.firstNum !== "" && this.operation === "") {
+      if (parseFloat(this.firstNum) > 0) {
+        return (this.firstNum = "-" + this.firstNum);
+      }
+      return (this.firstNum = this.firstNum.slice(1));
+    }
+    if (this.secondNum !== "" && this.operation !== "") {
+      if (parseFloat(this.secondNum) > 0) {
+        return (this.secondNum = "-" + this.secondNum);
+      }
+      return (this.secondNum = this.secondNum.slice(1));
+    }
+  };
+
   this.chooseOperation = (operation) => {
     if (this.firstNum === "") {
       return;
