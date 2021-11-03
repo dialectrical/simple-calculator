12dc9df38741fddb76a249a21327449fe44451c0
dialectrical <michael.camele@gmail.com>
2021-11-03 14:58:54 -0700

    feat: added theme1

---
+++ src/components/styles/theme.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const theme1 = {
+  bg: "hsl(222, 26%, 31%)",
+  keypadBg: "hsl(223, 31%, 20%)",
+  screenBg: "hsl(224, 36%, 15%)",
+  keyBg: "hsl(225, 21%, 49%)",
+  keyShadow: "hsl(224, 28%, 35%)",
+  keyBgToggle: "hsl(6, 63%, 50%)",
+  keyShadowToggle: "hsl(6, 70%, 34%)",
+  keyBgSecondary: "hsl(30, 25%, 89%)",
+  keyShadowSecondary: "hsl(28, 16%, 65%)",
+  primaryText: "hsl(221, 14%, 31%)",
+  secondaryText: "hsl(0, 0, 100%)",
+};
